61c283eb3717cd7393a9299281ace83260073ff9
shipp11 <shippdaddy1123@gmail.com>
2017-11-26 11:46:06 -0500

    counter1

---
+++ counter1.rb
@@ -0,0 +1,9 @@
+$counterAr = []
+
+def counterArClass()
+	$counterAr.class
+end
+
+def counterClass(test_number)
+	test_number.class
+end
---
+++ countertext1.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require_relative "counter1.rb"
+class TestCounter < Minitest::Test
+
+	def test_assert_1_is_1
+		assert_equal(1, 1)
+	end
+
+	def test_assert_that_counter_ar_is_an_array
+		assert_equal(Array, counterArClass())
+	end
+
+	def test_assert_that_counter_is_an_integer
+		assert_equal(Integer, counterClass(1))
+	end
+
+end
